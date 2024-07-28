85881dc1994df734402ac8edef29abd5bf57820f
Vitalii <vitaliti.yurchenko@gmail.com>
2024-07-28 19:08:41 +0300

    Add correction

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,7 @@
 package app;
 
 public class Main {
-
-    static int NUMBER_ORDER;
+    
     static String PRODUCT = "Product";
     static String CURRENCY = "EUR";
 
@@ -10,35 +9,36 @@ public class Main {
 
         int day;
         int pcs;
-        double cost;
+        int numberOrder = 0;
+        double price;
         String productName;
         day = 5;
         pcs = 41;
-        cost = 296.424634;
-        NUMBER_ORDER++;
+        price = 296.42;
+        numberOrder++;
         productName = "smartphone";
-        double amountOfSales = pcs * cost;
-        double avgCost = amountOfSales / day;
+        double cost = pcs * price;
+        double avgCost = cost / day;
 
         System.out.printf("%s No %s: %s,%n" +
                         "total sales for %s days is %s %.2f,%n" +
                         "sales by day is %s %.2f.%n",
-                PRODUCT, NUMBER_ORDER, productName, day,
-                CURRENCY, amountOfSales, CURRENCY, avgCost);
+                PRODUCT, numberOrder, productName, day,
+                CURRENCY, cost, CURRENCY, avgCost);
 
         day = 7;
         pcs = 85;
-        cost = 123.374706;
-        NUMBER_ORDER++;
+        price = 123.37;
+        numberOrder++;
         productName = "laptop";
-        amountOfSales = pcs * cost;
-        avgCost = amountOfSales / day;
+        cost = pcs * price;
+        avgCost = cost / day;
 
         System.out.printf("%s No %s: %s,%n" +
                         "total sales for %s days is %s %.2f,%n" +
                         "sales by day is %s %.2f.%n",
-                PRODUCT, NUMBER_ORDER, productName, day,
-                CURRENCY, amountOfSales, CURRENCY, avgCost);
+                PRODUCT, numberOrder, productName, day,
+                CURRENCY, cost, CURRENCY, avgCost);
 
     }
 }
